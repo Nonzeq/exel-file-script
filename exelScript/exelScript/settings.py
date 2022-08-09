a2a2d745eb9c35dbf63dc775eef7bd39501edefa
Bogdan <bgdn.kobylchak@gmail.com>
2022-08-09 18:21:09 +0300

    make it better

--- exelScript/exelScript/settings.py
+++ exelScript/exelScript/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -142,4 +142,5 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('API_KEY'),
     'API_SECRET': os.environ.get('API_SECRET'),
 }
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
